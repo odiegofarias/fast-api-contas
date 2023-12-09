990e29586d6f94be7dd2692e96328f2161508326
odiegofarias <diego.farias.fernandes@gmail.com>
2023-12-09 14:48:51 -0300

    Criando um router para post e acrescentando classes de models para simular um banco de dados ficticio

--- contas_a_pagar_e_receber/routers/contas_a_pagar_e_receber_router.py
+++ contas_a_pagar_e_receber/routers/contas_a_pagar_e_receber_router.py
@@ -1,13 +1,48 @@
+from decimal import Decimal
+from typing import List
 from fastapi import APIRouter
+from pydantic import BaseModel
 
 
 router = APIRouter(prefix='/contas-a-pagar-e-receber')
 
 
-@router.get('/')
+class ContaPagarReceberResponse(BaseModel):
+    id: int
+    descricao: str
+    valor: Decimal
+    tipo: str # PAGAR e RECEBER
+
+
+class ContaPagarReceberRequest(BaseModel):
+    descricao: str
+    valor: Decimal
+    tipo: str # PAGAR e RECEBER
+
+
+@router.get('/', response_model = List[ContaPagarReceberResponse])
 def listar_contas() -> list:
     return [
-        {'conta1': 'conta1'},
-        {'conta1': 'conta1'},
-        {'conta1': 'conta1'},
+        ContaPagarReceberResponse(
+            id = 1,
+            descricao = 'Aluguel',
+            valor = 1000.50,
+            tipo = "PAGAR"
+        ),
+        ContaPagarReceberResponse(
+            id = 2,
+            descricao = 'Salário',
+            valor = 5000,
+            tipo = "RECEBER"
+        ),
     ]
+
+
+@router.post('/', response_model=ContaPagarReceberResponse, status_code=201)
+def criar_conta(conta: ContaPagarReceberRequest):
+    return ContaPagarReceberResponse(
+            id = 3,
+            descricao = conta.descricao,
+            valor = conta.valor,
+            tipo = conta.tipo
+    )
