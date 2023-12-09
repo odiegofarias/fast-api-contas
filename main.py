98528ca16d584acb60ebbfbe5897cda4a1e73732
odiegofarias <diego.farias.fernandes@gmail.com>
2023-12-09 20:53:25 -0300

    Configurando migrações com o alembic

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 from fastapi import FastAPI
 from contas_a_pagar_e_receber.routers import contas_a_pagar_e_receber_router
-# from shared.database import Base, engine
 
-from contas_a_pagar_e_receber.models.contas_pagar_receber_models import ContaPagarReceber
-
-
-# Base.metadata.drop_all(bind=engine)
-# Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
