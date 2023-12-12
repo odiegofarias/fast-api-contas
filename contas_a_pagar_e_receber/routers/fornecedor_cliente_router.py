107a32db79c30773090dab3e94301d1cd87fc43c
odiegofarias <diego.farias.fernandes@gmail.com>
2023-12-12 20:00:03 -0300

    Criado CRUD completo com testes para router fornecedor-cliente

--- contas_a_pagar_e_receber/routers/fornecedor_cliente_router.py
+++ contas_a_pagar_e_receber/routers/fornecedor_cliente_router.py
@@ -4,8 +4,10 @@ from typing import List
 from contas_a_pagar_e_receber.models.fornecedor_cliente_model import FornecedorCliente
 
 from sqlalchemy.orm import Session
+from contas_a_pagar_e_receber.routers.contas_a_pagar_e_receber_router import busca_conta_por_id
 
 from shared.dependencies import get_db
+from shared.exceptions import NotFound
 
 
 router = APIRouter(prefix='/fornecedor-cliente')
@@ -15,13 +17,54 @@ class FornecedorClienteResponse(BaseModel):
     nome: str
 
     class Config:
-        orm_mode = True
+        from_attributes = True
 
 
-class FornecedorClienteResponse(BaseModel):
+class FornecedorClienteRequest(BaseModel):
     nome: str = Field(min_length=3, max_length=255)
 
 
 @router.get('', response_model = List[FornecedorClienteResponse])
 def listar_fornecedor_cliente(db: Session = Depends(get_db), skip: int = 0, limit: int = 100) -> List[FornecedorClienteResponse]:
     return db.query(FornecedorCliente).offset(skip).limit(limit).all()
+
+@router.post('', response_model=FornecedorClienteResponse, status_code=201)
+def criar_fornecedor_cliente(fornecedor_cliente_request: FornecedorClienteRequest, db: Session = Depends(get_db)) -> FornecedorClienteResponse:
+    fornecedor_cliente = FornecedorCliente(
+        **fornecedor_cliente_request.model_dump()
+    )
+
+    db.add(fornecedor_cliente)
+    db.commit()
+    db.refresh(fornecedor_cliente)
+
+    return fornecedor_cliente
+
+@router.put('/{id}', response_model=FornecedorClienteResponse, status_code=200)
+def atualiza_fornecedor_cliente(
+    id: int, fornecedor_cliente_request: FornecedorClienteRequest,
+    db: Session = Depends(get_db)
+) -> FornecedorClienteResponse:
+    fornecedor_cliente = busca_fornecedor_cliente_por_id(id, db)
+    fornecedor_cliente.nome = fornecedor_cliente_request.nome
+
+    db.add(fornecedor_cliente)
+    db.commit()
+    db.refresh(fornecedor_cliente)
+
+    return fornecedor_cliente
+
+def busca_fornecedor_cliente_por_id(id_fornecedor_cliente: int, db: Session):
+    fornecedor_cliente: FornecedorCliente = db.query(FornecedorCliente).get(id_fornecedor_cliente)
+
+    if fornecedor_cliente is None:
+        raise NotFound('Fornecedor/cliente')
+    
+    return fornecedor_cliente
+
+@router.delete('/{id}', status_code=204)
+def exclui_fornecedor_cliente(id: int, db: Session = Depends(get_db)) -> None:
+    fornecedor_cliente = busca_fornecedor_cliente_por_id(id, db)
+
+    db.delete(fornecedor_cliente)
+    db.commit()
---
+++ tests/contas_a_pagar_e_receber/routers/test_integrado_fornecedor_cliente_router.py
@@ -0,0 +1,99 @@
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from main import app
+from shared.database import Base
+from shared.dependencies import get_db
+
+
+client = TestClient(app)
+
+SQLALCHEMY_DATABASE_URL = 'sqlite:///sqlite3.db'
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={
+        'check_same_thread': False,
+    },
+)
+
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def override_get_db():
+    try:
+        db = TestingSessionLocal()
+        yield db
+    finally:
+        db.close()
+
+app.dependency_overrides[get_db] = override_get_db
+
+
+def test_deve_criar_funcionario_cliente():
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+
+    response = client.post('/fornecedor-cliente', json={
+        'nome': 'CELPE'
+    })
+
+    assert response.status_code == 201
+    assert response.json()['id'] == 1
+    assert response.json() == {'id': 1, 'nome': 'CELPE'}
+
+def test_deve_listar_fornecedores_clientes():
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+
+    client.post('/fornecedor-cliente', json={
+        'nome': 'BBG TELECOM'
+    })
+
+    client.post('/fornecedor-cliente', json={
+        'nome': 'TIM SA'
+    })
+
+    response = client.get('/fornecedor-cliente')
+
+    assert response.status_code == 200
+    assert len(response.json()) == 2
+    assert response.json() == [
+        {'id': 1, 'nome': 'BBG TELECOM'},
+        {'id': 2, 'nome': 'TIM SA'}
+    ]
+
+def test_deve_atualizar_fornecedor_cliente_corretamente():
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+
+    data_json = {
+        'nome': 'VIVO SA'
+    }
+
+    response_post = client.post('/fornecedor-cliente', json=data_json)
+
+    id_fornecedor_cliente = response_post.json()['id']
+
+    response_put = client.put(f'/fornecedor-cliente/{id_fornecedor_cliente}', json={
+        'nome': 'BBG TELECOM'
+    })
+
+    assert response_put.status_code == 200
+    assert response_put.json()['nome'] == 'BBG TELECOM'
+
+def test_deve_remover_fornecedor_cliente():
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+
+    data_json = {'nome': 'VIVO SA'}
+
+    response_post = client.post('/fornecedor-cliente', json=data_json)
+
+    id_fornecedor_cliente = response_post.json()['id']
+
+    response_delete = client.delete(f'/fornecedor-cliente/{id_fornecedor_cliente}')
+
+    response_get = client.get('/fornecedor-cliente')
+    assert response_delete.status_code == 204
+    assert response_get.json() == []
+    
