5e35980804b0f20d5c6cd3cbefcd40cfed9e16f7
odiegofarias <diego.farias.fernandes@gmail.com>
2023-12-23 18:21:30 -0300

    Criado teste para verificar se contas de fornecedors clientes está retornando as contas do fornecedor específico

--- tests/contas_a_pagar_e_receber/routers/test_integrado_contas_de_fornecedor_cliente.py
+++ tests/contas_a_pagar_e_receber/routers/test_integrado_contas_de_fornecedor_cliente.py
@@ -33,11 +33,15 @@ def test_deve_retornar_as_contas_associadas_a_um_fornecedor_cliente():
     Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
 
-    novo_fornecedor = {
+    novo_fornecedor_1 = {
+        'nome': 'BBG TELECOM'
+    }
+    novo_fornecedor_2 = {
         'nome': 'BBG TELECOM'
     }
 
-    client.post('/fornecedor-cliente', json=novo_fornecedor)
+    client.post('/fornecedor-cliente', json=novo_fornecedor_1)
+    client.post('/fornecedor-cliente', json=novo_fornecedor_2)
 
     nova_conta_1 = {
         "descricao": "Curso de Python",
@@ -45,10 +49,37 @@ def test_deve_retornar_as_contas_associadas_a_um_fornecedor_cliente():
         "tipo": "PAGAR",
         "fornecedor_cliente_id": 1
     }
+    nova_conta_2 = {
+        "descricao": "Curso de FastAPI",
+        "valor": 459.99,
+        "tipo": "PAGAR",
+        "fornecedor_cliente_id": 1
+    }
+    nova_conta_3 = {
+        "descricao": "Curso de Investimentos",
+        "valor": 997.97,
+        "tipo": "PAGAR",
+        "fornecedor_cliente_id": 2
+    }
 
-    response = client.post('/contas-a-pagar-e-receber', json=nova_conta_1)
+    response_conta_1 = client.post('/contas-a-pagar-e-receber', json=nova_conta_1)
+    response_conta_2 = client.post('/contas-a-pagar-e-receber', json=nova_conta_2)
+    response_conta_3 = client.post('/contas-a-pagar-e-receber', json=nova_conta_3)
 
-    assert response.status_code == 201
-    assert response.json() == {'id': 1, 'descricao': 'Curso de Python', 'valor': '1350.9900000000', 'tipo': 'PAGAR', 'fornecedor': {'id': 1, 'nome': 'BBG TELECOM'}}
 
+    response_get = client.get('/fornecedor-cliente/1/contas-a-pagar-e-receber')
+
+    assert response_conta_1.status_code == 201
+    assert response_conta_2.status_code == 201
+    assert response_conta_3.status_code == 201
+
+    assert response_get.json() == [
+            {'id': 1, 'descricao': 'Curso de Python', 'valor': '1350.9900000000', 'tipo': 'PAGAR', 'fornecedor': {'id': 1, 'nome': 'BBG TELECOM'}}, 
+            {'id': 2, 'descricao': 'Curso de FastAPI', 'valor': '459.9900000000', 'tipo': 'PAGAR', 'fornecedor': {'id': 1, 'nome': 'BBG TELECOM'}}
+        ]
+    
+
+
+
+    
 
